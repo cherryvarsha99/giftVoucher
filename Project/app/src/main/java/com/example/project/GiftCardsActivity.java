0b188d220432b5610141145e3167b6b22cae1695
cherryvarsha99 <S540114@nwmissouri.edu>
2021-03-25 11:27:21 -0500

    committing shared prefrences Utility- varsha

--- Project/app/src/main/java/com/example/project/GiftCardsActivity.java
+++ Project/app/src/main/java/com/example/project/GiftCardsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GiftCard_Item extends AppCompatActivity {
+public class GiftCardsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
