138458ae378c2738dbdec154bcf07f1b8fe7d551
Sreenidhi Madala <S541226@nwmissouri.edu>
2021-03-25 00:26:48 -0500

    changes made for Redeem_Fragment - sreenidhi madala

--- Project/app/src/main/java/com/example/project/Redeem_Fragment.java
+++ Project/app/src/main/java/com/example/project/Redeem_Fragment.java
@@ -9,6 +9,6 @@ public class Redeem_Fragment extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redeemCard__fragment);
+        setContentView(R.layout.activity_redeemcard__fragment);
     }
 }
