ef4ab214c8cd3787f7dec3906c8368e09d0ce2da
Sreenidhi Madala <S541226@nwmissouri.edu>
2021-03-25 11:09:23 -0500

    Added RedeemCallback.java - Sreenidhi Madala Created utils package and Added ViewUtils.java - Sreenidhi Madala

---
+++ Project/app/src/main/java/com/example/project/interfaces/RedeemCallback.java
@@ -0,0 +1,11 @@
+package com.example.project.interfaces;
+
+/**
+ * Created by Maureen Sindiso Mpofu  on 17/10/2020.
+ */
+public interface RedeemCallback {
+
+    void update( String id);
+    void swapToPurchase();
+
+}
---
+++ Project/app/src/main/java/com/example/project/utils/ViewUtils.java
@@ -0,0 +1,15 @@
+package com.example.project.utils;
+
+import android.util.Log;
+
+public class ViewUtils {
+
+    private static final String TAG = ;
+
+    //logger
+        public static void logErrorMessage(String errorMessage) {
+            Log.d(TAG, errorMessage);
+        }
+
+    }
+
