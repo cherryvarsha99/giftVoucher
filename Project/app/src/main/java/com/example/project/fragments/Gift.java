05548eb3b290984006f2d8fd471e186241954e96
Sreenidhi Madala <S541226@nwmissouri.edu>
2021-03-25 10:57:26 -0500

    Added PasswordChange.java - Sreenidhi Madala Added Gift.java - Sreenidhi Madala Added RedeemCardFragment.java - Sreenidhi Madala deleted ChangePasswordActivity.java - Sreenidhi Madala modified EditProfileActivity.java - Sreenidhi Madala

---
+++ Project/app/src/main/java/com/example/project/fragments/Gift.java
@@ -0,0 +1,17 @@
+package com.example.project.fragments;
+/**
+ * Created by Maureen Sindiso Mpofu  on 6/3/2021.
+ */
+public class Gift {
+
+    private String id;
+    private String message;
+    private String amount;
+    private String currency;
+    private String pin;
+    private String code;
+    private String redeemed;
+    private String userId;
+
+
+   }
---
+++ Project/app/src/main/java/com/example/project/fragments/RedeemCardFragment.java
@@ -0,0 +1,29 @@
+package com.example.project.fragments;
+
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+public class RedeemCardFragment extends Fragment implements View.OnClickListener {
+
+    private TextInputEditText editTextMessage;
+    private TextInputEditText editTextPin;
+    private TextInputLayout textInputLayoutPin;
+    private TextInputLayout textInputLayoutCode;
+    private TextInputEditText editTextCode;
+    private Button buttonRedeem;
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
