43809f20c3d1da655f92b464e5abea87a65dff23
cherryvarsha99 <S540114@nwmissouri.edu>
2021-03-25 11:24:17 -0500

    committing CurrencyConversion Util- varsha

---
+++ Project/app/src/main/java/com/example/project/utils/CurrencyConversion.java
@@ -0,0 +1,7 @@
+package com.example.project.utils;
+
+import java.text.DecimalFormat;
+
+public class CurrencyConversion {
+    public static DecimalFormat decimalFormatTo2 = new DecimalFormat("#.##");
+}
