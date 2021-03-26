711ba5e3b9864103c29eeb7d70028477c28f5520
cherryvarsha99 <S540114@nwmissouri.edu>
2021-03-26 14:26:36 -0500

    added intent to  View Profile in MainMenuActivity.java to navigate Profile activity - varsha

--- Project/app/src/main/java/com/example/project/MainMenuActivity.java
+++ Project/app/src/main/java/com/example/project/MainMenuActivity.java
@@ -2,13 +2,18 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.project.authentication.ProfileActivity;
+
 public class MainMenuActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private Button btnViewProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +21,19 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         mTextView = (TextView) findViewById(R.id.text);
+        btnViewProfile= (Button) findViewById(R.id.btnViewProfile);
 
-        Intent intent = getIntent();
-        String value = intent.getStringExtra("key");
+
+
+
+
+
+    }
+
+    public void profile(View v)
+    {
+        Intent myIntent = new Intent(getApplicationContext(), ProfileActivity.class);
+
+        startActivity(myIntent);
     }
 }
