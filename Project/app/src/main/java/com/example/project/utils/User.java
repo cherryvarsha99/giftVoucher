857d95deb439622f46d53800a8165b89c97b31fd
Sreenidhi Madala <S541226@nwmissouri.edu>
2021-03-26 17:56:34 -0500

    updated users in utils/User.java - Sreenidhi Madala

--- Project/app/src/main/java/com/example/project/utils/User.java
+++ Project/app/src/main/java/com/example/project/utils/User.java
@@ -1,68 +1,64 @@
 package com.example.project.utils;
 
+/**
+ * Created by Maureen Sindiso Mpofu  on 6/3/2021.
+ */
+public class User {
 
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 
-    /**
-     * Created by Maureen Sindiso Mpofu  on 6/3/2021.
-     */
-    public class User {
-
-        private String id;
-        private String firstname;
-        private String lastname;
-        private String email;
-        private String password;
-
-        public User(String id, String firstname, String lastname, String email, String password) {
-            this.id = id;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.email = email;
-            this.password = password;
-        }
-
-        public User() {
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getFirstname() {
-            return firstname;
-        }
-
-        public void setFirstname(String firstname) {
-            this.firstname = firstname;
-        }
-
-        public String getLastname() {
-            return lastname;
-        }
-
-        public void setLastname(String lastname) {
-            this.lastname = lastname;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public User(String id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
     }
 
+    public User() {
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
