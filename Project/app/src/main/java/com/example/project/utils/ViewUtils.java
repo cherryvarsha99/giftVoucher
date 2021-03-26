6365314f85904f28e905a2be508eb81fb50b8909
Sreenidhi Madala <S541226@nwmissouri.edu>
2021-03-26 17:54:43 -0500

    Updated View utils/ViewUtils.java - Sreenidhi Madala

--- Project/app/src/main/java/com/example/project/utils/ViewUtils.java
+++ Project/app/src/main/java/com/example/project/utils/ViewUtils.java
@@ -1,15 +1,483 @@
 package com.example.project.utils;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.text.format.DateUtils;
+import android.text.format.Time;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.project.R;
+
+import java.net.URL;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class ViewUtils {
 
-    private static final String TAG = "ProjectUtility";
 
     //logger
-        public static void logErrorMessage(String errorMessage) {
-            Log.d(TAG, errorMessage);
+    public static void logErrorMessage(String errorMessage) {
+        Log.d(TAG, errorMessage);
+    }
+
+    //Fragments Tags
+    public static final String SignInFragment = "Login";
+    public static final String SignUpFragment = "Register";
+    public static final String ForgotPassFragment = "Forgot Password";
+    public static final String VerifyAccountFragment = "Verify Account";
+    public static final String ProfileFragment = "Settings";
+    public static final String EditProfileFragment = "Edit Profile";
+    public static final String TermsOfServiceFragment = "Terms of service";
+    public static final String GetSupportFragment = "Get support";
+    public static final String ChangePassFragment = "Change Password";
+
+    public static final String TAG = "ProjectUtility";
+    private static AlertDialog dialog;
+    private static ProgressDialog mProgressDialog;
+    private static final String VERSION_UNAVAILABLE = "N/A";
+    private static Dialog dialog_gif;
+
+    public static Bitmap bmp;
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    //For Changing Status Bar Color if Device is above 5.0(Lollipop)
+    public static void changeStatusBarColor(Activity activity) {
+
+        Window window = activity.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(activity.getResources().getColor(R.color.design_default_color_primary_dark));
         }
+    }
+
+    public static void changeStatusBarColorNew(Activity activity, int color) {
+        Window window = activity.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(activity.getResources().getColor(color));
+        }
+    }
+
+    public static void Fullscreen(Activity activity) {
+        activity.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+
+    public static void statusbarBackgroundTrans(Activity activity, int drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = activity.getResources().getDrawable(drawable);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            // window.setNavigationBarColor(activity.getResources().getColor(R.color.transparent));
+            window.setBackgroundDrawable(background);
+        }
+    }
+
+    public static void statusbarBackgroundTransformURL(Activity activity, String imageurl) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            URL url = null;
+            try {
+                url = new URL(imageurl);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Window window = activity.getWindow();
+            Drawable background = new BitmapDrawable(activity.getResources(), bmp);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            // window.setNavigationBarColor(activity.getResources().getColor(R.color.transparent));
+            window.setBackgroundDrawable(background);
+        }
+    }
+
+    public static boolean hasPermissionInManifest(Activity activity, int requestCode, String permissionName) {
+        if (ContextCompat.checkSelfPermission(activity,
+                permissionName)
+                != PackageManager.PERMISSION_GRANTED) {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{permissionName},
+                    requestCode);
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean isPriceBid(String number, String num) {
+        //	Log.d("tag", "Number is not valid");
+        return !num.equals(number);
+    }
+
+
+    //For Progress Dialog
+    public static ProgressDialog getProgressDialog(Context context) {
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("please_wait");
+        return progressDialog;
+    }
+
+
+    public static boolean containsOnlyNumbers(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+
+    // For Alert Dialog in App
+    public static Dialog createDialog(Context context, int titleId, int messageId,
+                                      DialogInterface.OnClickListener positiveButtonListener,
+                                      DialogInterface.OnClickListener negativeButtonListener) {
+        Builder builder = new Builder(context);
+        builder.setTitle(titleId);
+        builder.setMessage(messageId);
+        builder.setPositiveButton("ok", positiveButtonListener);
+        builder.setNegativeButton("cancel", negativeButtonListener);
+
+        return builder.create();
+    }
+
+    // For Alert Dialog on Custom View in App
+    public static Dialog createDialog(Context context, int titleId, int messageId, View view,
+                                      DialogInterface.OnClickListener positiveClickListener,
+                                      DialogInterface.OnClickListener negativeClickListener) {
+
+        Builder builder = new Builder(context);
+        builder.setTitle(titleId);
+        builder.setMessage(messageId);
+        builder.setView(view);
+        builder.setPositiveButton("ok", positiveClickListener);
+        builder.setNegativeButton("cancel", negativeClickListener);
+
+        return builder.create();
+    }
+
+
+    public static void showDialog(Context context, String title, String msg,
+                                  DialogInterface.OnClickListener OK, boolean isCancelable) {
+        if (title == null)
+            title = context.getResources().getString(R.string.app_name);
+        if (OK == null)
+            OK = new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    hideDialog();
+                }
+            };
+        if (dialog == null) {
+            Builder builder = new Builder(context);
+            builder.setTitle(title);
+            builder.setMessage(msg);
+            builder.setPositiveButton("OK", OK);
+            dialog = builder.create();
+            dialog.setCancelable(isCancelable);
+        }
+        try {
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Static method to show the dialog with custom message on it
+     *
+     * @param context      Context of the activity where to show the dialog
+     * @param title        Title to be shown either custom or application name
+     * @param msg          Custom message to be shown on dialog
+     * @param OK           Overridden click listener for OK button in dialog
+     * @param cancel       Overridden click listener for cancel button in dialog
+     * @param isCancelable : Sets whether this dialog is cancelable with the BACK key.
+     */
+    public static void showDialog(Context context, String title, String msg,
+                                  DialogInterface.OnClickListener OK,
+                                  DialogInterface.OnClickListener cancel, boolean isCancelable) {
+
+        if (title == null)
+            title = context.getResources().getString(R.string.app_name);
+
+        if (OK == null)
+            OK = new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface paramDialogInterface,
+                                    int paramInt) {
+                    hideDialog();
+                }
+            };
+
+        if (cancel == null)
+            cancel = new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface paramDialogInterface,
+                                    int paramInt) {
+                    hideDialog();
+                }
+            };
+
+        if (dialog == null) {
+            Builder builder = new Builder(context);
+            builder.setTitle(title);
+            builder.setMessage(msg);
+            builder.setPositiveButton("OK", OK);
+            builder.setNegativeButton("Cancel", cancel);
+            dialog = builder.create();
+            dialog.setCancelable(isCancelable);
+        }
+        try {
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Static method to show the progress dialog.
+     *
+     * @param context      : Context of the activity where to show the dialog
+     * @param isCancelable : Sets whether this dialog is cancelable with the BACK key.
+     * @param message      : Message to be shwon on the progress dialog.
+     * @return Object of progress dialog.
+     */
+    public static Dialog showProgressDialog(Context context,
+                                            boolean isCancelable, String message) {
+        mProgressDialog = new ProgressDialog(context);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setMessage(message);
+        mProgressDialog.show();
+        mProgressDialog.setCancelable(isCancelable);
+        mProgressDialog.setIndeterminate(true);
+        //mProgressDialog.setIndeterminateDrawable(context.getResources().getDrawable(R.drawable.my_animation));
+        return mProgressDialog;
+    }
+    /**
+     * Static method to pause the progress dialog.
+     */
+    public static void pauseProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                mProgressDialog.cancel();
+                mProgressDialog.dismiss();
+                mProgressDialog = null;
+            }
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * show Toast
+     */
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 
     }
 
+    /*Show Log Error*/
+    public static void showLog(String tag, String message) {
+        Log.e(tag, message);
+    }
+    /**
+     * Static method to cancel the Dialog.
+     */
+    public static void cancelDialog() {
+        try {
+            if (dialog != null) {
+                dialog.cancel();
+                dialog.dismiss();
+                dialog = null;
+            }
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Static method to hide the dialog if visible
+     */
+    public static void hideDialog() {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+            dialog.cancel();
+            dialog = null;
+        }
+    }
+
+
+    /**
+     * Checks the validation of email address.
+     * Takes pattern and checks the text entered is valid email address or not.
+     *
+     * @param email : email in string format
+     * @return True if email address is correct.
+     */
+    public static boolean isEmailValid(String email) {
+        String expression = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            return true;
+        } else if (email.equals("")) {
+            return false;
+        }
+        return false;
+    }
+
+    public static boolean urlValidate(String url) {
+        String expression = ".*(youtube|youtu.be).*";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.matches()) {
+            return true;
+        } else if (url.equals("")) {
+            return false;
+        }
+        return false;
+
+    }
+
+
+    /**
+     * Method checks if the given phone number is valid or not.
+     *
+     * @param number : Phone number is to be checked.
+     * @return True if the number is valid.
+     * False if number is not valid.
+     */
+    public static boolean isPhoneNumberValid(String number) {
+        //	Log.d("tag", "Number is not valid");
+        return number.length() >= 4 && number.length() <= 16;
+    }
+
+    public static boolean isPasswordLengthCorrect(EditText text) {
+        return text.getText() != null || text.getText().toString().trim().length() >= 6;
+    }
+
+
+    public static boolean isPasswordValid(String number) {
+        String regexStr = " (?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,20})$";
+        return number.matches(regexStr);
+    }
+
+    public static long currentTimeInMillis() {
+        Time time = new Time();
+        time.setToNow();
+        return time.toMillis(false);
+    }
+
+    /**
+     * Checks if any text box is null or not.
+     *
+     * @param text : Text view for which validation is to be checked.
+     * @return True if not null.
+     */
+    public static String getEditTextValue(EditText text) {
+        return text.getText().toString().trim();
+    }
+
+    /**
+     * Checks if any text box is null or not.
+     *
+     * @param text : Text view for which validation is to be checked.
+     * @return True if not null.
+     */
+    public static boolean isEditTextFilled(EditText text) {
+        return text.getText() != null && text.getText().toString().trim().length() > 0;
+    }
+
+    public static boolean isEditTextFilled(EditText[] editTexts) {
+        for(EditText editText:editTexts){
+            if(!isEditTextFilled(editText)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isNetworkConnected(Context mContext) {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        // There are no active networks.
+        return ni != null;
+    }
+
+    public static void InternetAlertDialog(Context mContext) {
+        Builder alertDialog = new Builder(mContext);
+
+        //Setting Dialog Title
+        alertDialog.setTitle("Error Connecting");
+
+        //Setting Dialog Message
+        alertDialog.setMessage("No Internet Connection");
+
+        //On Pressing Setting button
+        alertDialog.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+
+                    }
+                });
+        alertDialog.show();
+    }
+    public static String convertTimestampToTime(long timestamp) {
+        Timestamp tStamp = new Timestamp(timestamp);
+        SimpleDateFormat simpleDateFormat;
+        if (DateUtils.isToday(timestamp)) {
+            simpleDateFormat = new SimpleDateFormat("hh:mm a");
+            return simpleDateFormat.format(tStamp);
+        } else {
+            simpleDateFormat = new SimpleDateFormat("hh:mm a");
+            return simpleDateFormat.format(tStamp);
+        }
+    }
+
+    public static String convertTimestampDateToTime(long timestamp) {
+        Timestamp tStamp = new Timestamp(timestamp);
+        SimpleDateFormat simpleDateFormat;
+        simpleDateFormat = new SimpleDateFormat("dd MMM, yyyy hh:mm a");
+        return simpleDateFormat.format(tStamp);
+    }
+}
