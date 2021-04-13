8aa9223fa7a85507d35a0bd9002c4e529f00c9c3
cherryvarsha99 <S540114@nwmissouri.edu>
2021-04-13 14:12:02 -0500

    changed login Intent and added fregments in oncreate- varsha

--- Project/app/src/main/java/com/example/project/authentication/LoginActivity.java
+++ Project/app/src/main/java/com/example/project/authentication/LoginActivity.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project.MainMenuActivity;
@@ -20,27 +18,42 @@ import com.example.project.utils.SharedPrefs;
 
 
 
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SharedPrefs sharedPrefs;
     EditText emailEt, passwordEt;
     CheckBox checkBox;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activty);
+        sharedPrefs =SharedPrefs.getInstance(this);
+        findViewById(R.id.btnLogin).setOnClickListener(this);
+        findViewById(R.id.txtForgotPasswordLink).setOnClickListener(this);
+        findViewById(R.id.txtRegisterLink).setOnClickListener(this);
+        CheckBox checkBox = findViewById(R.id.checkBoxShowHidePassword);
+        emailEt= findViewById(R.id.editTextEmail);
+        passwordEt = findViewById(R.id.editTextPassword);
+        checkBox.setOnCheckedChangeListener((button, isChecked) -> {
+            // If it is checked then show password else hide password
+            if (isChecked) { //show password
+                checkBox.setText("hide");
+                passwordEt.setInputType(InputType.TYPE_CLASS_TEXT);
+                passwordEt.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else { //hide password
+                checkBox.setText("show");
+                passwordEt.setInputType(InputType.TYPE_CLASS_TEXT
+                        | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                passwordEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
     }
 
-
-    public void login(View v) {
-
-
-            Intent myIntent = new Intent(getApplicationContext(), MainMenuActivity.class);
-
-            startActivity(myIntent);
-
-
+    @Override
+    public void onClick(View v) {
 
     }
 }
